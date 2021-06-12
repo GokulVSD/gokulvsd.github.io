98c313955f20c63ffeaf4e4c147a2ec0c7c43cbc
GokulVSD <gokul.vasda@gmail.com>
2021-06-12 18:04:48 +0530

    clear

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const darkcss = document.getElementById("darkcss");
+if (localStorage.getItem('theme') === 'light') {
+        darkcss.disabled = "disabled";
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    function switchTheme(e) {
+        const darkcss = document.getElementById("darkcss");
+        if (e.target.checked) {
+            darkcss.disabled = "disabled";
+            localStorage.setItem('theme', "light");
+        } else {
+            darkcss.disabled = undefined;
+            localStorage.setItem('theme', "dark");
+        }
+    }
+    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+    if (toggleSwitch) {
+        toggleSwitch.addEventListener('change', switchTheme, false);
+        if (localStorage.getItem('theme') === 'light') {
+            toggleSwitch.checked = "checked";
+        }
+    }
+});
